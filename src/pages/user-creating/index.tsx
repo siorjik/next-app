409595c76c0f66022cce27a68da51057a3886496
siorjik <serlkaplun@gmail.com>
2023-05-26 19:16:20 +0300

    added registration logic

---
+++ src/pages/user-creating/index.tsx
@@ -0,0 +1,70 @@
+import { Input, Form, Button, Alert } from "antd"
+import { useState } from "react"
+import Link from 'next/link'
+import { getSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next'
+
+import apiService from "@/services/apiService"
+import Error from "@/components/Error"
+import { localApiUserCreatePath, loginAppPath } from '@/utils/paths'
+import { ApiErrorType } from '@/types/errorType'
+
+const { Item } = Form
+
+const UserCreating = () => {
+  const [err, setErr] = useState<ApiErrorType>({ message: '', statusCode: 0, error: '' })
+  const [isShowAlert, setShowAlert] = useState(false)
+
+  const [form] = Form.useForm()
+
+  const onSubmit = async (values: { [k: string]: string | number }) => {
+    const result = await apiService.post(localApiUserCreatePath, values)
+    
+    if (result.error) setErr(result)
+    else {
+      setErr({ message: '', statusCode: 0, error: '' })
+      setShowAlert(true)
+
+      form.resetFields()
+    }
+  }
+
+  return (
+    <div className='primary-form'>
+      <h2>New account creating</h2>
+
+      <Form form={form} layout='vertical' onFinish={onSubmit}>
+        <Item name='firstName' label='First Name' rules={[{ required: true, message: 'Please input your Last Name!' }]}>
+          <Input />
+        </Item>
+        <Item name='lastName' label='Last Name' rules={[{ required: true, message: 'Please input your Last Name!' }]}>
+          <Input />
+        </Item>
+        <Item 
+          name='email' 
+          label='Email' 
+          rules={[{ required: true, message: 'Please input your Email!' }, { type: 'email', message: 'The input is not valid E-mail!' }]}
+        >
+          <Input type='email' />
+        </Item>
+        <Item><Button type='primary' htmlType='submit'>Create</Button></Item>
+      </Form>
+
+      {err.error ? <Error error={err} className='w-50-percent' /> : null}
+
+      {isShowAlert && <Alert message='Account was created. Check your mail to continue...' type="success" showIcon />}
+
+      <Link className='pt-10' href={loginAppPath}>go to login page</Link>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx)
+
+  if (session) return { redirect: { destination: '/', permanent: false } }
+
+  return { props: {} }
+}
+
+export default UserCreating
--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,4 +1,6 @@
 export const loginAppPath = '/login'
+export const userCreatingAppPath = '/user-creating'
+export const passwordCreatingAppPath = '/password-creating'
 
 // user
 export const usersAppPath = '/users'
@@ -9,6 +11,7 @@ export const getUserUpdateAppPath = (id: number | string) => `${usersAppPath}/${
 /*-----------------local api paths---------------------------*/
 // user
 export const localApiUserCreatePath = '/api/user/create'
+export const localApiUserCreatePasswordPath = '/api/user/create-password'
 export const getLocalApiUserUpdatePath = (id: string) => `/api/user/${id}/update`
 export const getLocalApiUserDeletePath = (id: number) => `/api/user/${id}/delete`
 
@@ -21,6 +24,7 @@ export const apiLogoutPath = '/auth/logout'
 // user
 export const apiUsersPath = '/user'
 export const apiUserCreatePath = '/user/create'
+export const apiUserCreatePasswordPath = '/user/create-password'
 export const getApiUserPath = (id: string) => `${apiUsersPath}/${id}`
 export const getApiUserUpdatePath = (id: string) => `/user/${id}/update`
 export const getApiUserDeletePath = (id: number) => `/user/${id}/delete`
